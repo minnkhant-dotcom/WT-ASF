6630fde60cb0af557a4c108c4f13d9b7d0152307
Minn Khant <itsminnkhant@gmail.com>
2024-11-08 22:57:29 +0800

    complete overhaul based on what i've learned; added bootstrap library, add jquery; finished up structure of landing page - todo: more styling

--- media/js/script.js
+++ media/js/script.js
@@ -1,18 +1,5 @@
-function navButton() {
-  let x = document.getElementById("navBar0");
-  if (x.className === "navBar") {
-    x.className = "navBarNot";
-  } else {
-    x.className = "navBar";
-  }
-}
-
-
-window.addEventListener('scroll', () => {
-  let header = document.querySelector('header');
-  if (window.scrollY > 0) {
-      header.classList.add('header-scrolled');
-  } else {
-      header.classList.remove('header-scrolled');
-  }
+$(document).ready(function(){S
+    $("#logo").click(function() {
+        window.location.href = "another.html"
+    })
 });
