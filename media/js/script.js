72b3f96591c50fd75bf269bf73c9d078c249590b
Minn Khant <itsminnkhant@gmail.com>
2024-11-09 23:59:15 +0800

    cleaned up and established external css, js files; added footer to all pages

--- media/js/script.js
+++ media/js/script.js
@@ -2,4 +2,12 @@ $(document).ready(function(){S
     $("#logo").click(function() {
         window.location.href = "another.html"
     })
+
+    $('#newsletter-form').on('submit', function (e) {
+        e.preventDefault();
+        alert("Thank you for subscribing to our newsletter!");
+        $('#name').val('');
+        $('#email').val('');
+    });
+
 });
